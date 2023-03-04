e24afc96a7b3f42cc3e119671b94d48dc6a13cfa
MisterCxmpy <cosgunhussein@gmail.com>
2023-03-04 10:06:58 +0000

    add username check

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-const startBtn = document.querySelector("#start-btn")
-const nickNameInput = document.querySelector("#nick-name-input")
+const startBtn = document.querySelector("#start-btn");
+const nickNameInput = document.querySelector("#nick-name-input");
+const usernameModal = document.querySelector("#username-modal");
+const closeUserModalBtn = document.querySelector("#close-user-modal-btn")
 
 const getUser = (e) => {
-    const username = nickNameInput.value
+  const username = nickNameInput.value;
+  let data = localStorage.getItem("usernames");
+  let usernameData = data ? JSON.parse(data) : [];
 
-    if (!username) return;
+  if (!username) return;
 
-    localStorage.setItem('username', username);
+  const user = usernameData.find(
+    (user) => user.toLowerCase() == username.toLowerCase()
+  );
 
-    nickNameInput.value = ""
+  if (user) {
+    usernameModal.style.display = "block";
+  } else {
+    usernameData.push(username);
+    localStorage.setItem("usernames", JSON.stringify(usernameData));
+
+    localStorage.setItem("username", username);
+
+    nickNameInput.value = "";
 
     window.location.href = "home.html";
+  }
+};
+
+const closeUserModal = (e) => {
+    usernameModal.style.display = "none";
 }
 
 startBtn.addEventListener("click", getUser);
+closeUserModalBtn.addEventListener("click", closeUserModal)
