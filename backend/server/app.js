d3fe896b03eaa9ddf27ff0b4ec1ceac3f2c1cba2
SheiKha Ahm desk <itzshakeey@gmail.com>
2023-03-06 11:09:07 +0000

    added logger

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const logger = require("./logger")
 
+app.use(cors());
+app.use(express.json());
+app.use(logger);
 
+app.get("/", (req, res) => {
+    res.send(`This is the quiz API`)
+})
 
 
 module.exports = app;
