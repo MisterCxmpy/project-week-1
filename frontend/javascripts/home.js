8554d1a539ae2bda786f7dee59296dac670d1a7b
MisterCxmpy <cosgunhussein@gmail.com>
2023-03-06 14:21:32 +0000

    link buttons to quiz page

--- frontend/javascripts/home.js
+++ frontend/javascripts/home.js
@@ -1,13 +1,22 @@
 const eraBtns = document.querySelectorAll(".era-btn")
 const closeDifficultyModal = document.querySelector("#close-difficulty-modal-btn")
 const difficultyModal = document.querySelector("#difficulty-modal")
+const difficultyBtns = document.querySelectorAll(".difficulty-btn")
+
+let era = null
 
 const openModal = (e) => {
   difficultyModal.style.display = "block"
+  era = e.target.dataset.era;
 }
 
 const closeModal = (e) => {
   difficultyModal.style.display = "none"
+  era = null;
+}
+
+const loadQuizPage = (e) => {
+  //window.location.href = `quiz.html/${era}/${e.target.dataset.difficulty}`;
 }
 
 closeDifficultyModal.addEventListener("click", closeModal)
@@ -18,3 +27,6 @@ eraBtns.forEach(btn => {
   btn.addEventListener("click", openModal)
 });
 
+difficultyBtns.forEach(btn => {
+  btn.addEventListener("click", loadQuizPage)
+});
