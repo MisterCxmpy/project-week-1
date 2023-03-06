265ae36e5d6100eb146354311c9b939857e46cf2
SheiKha Ahm desk <itzshakeey@gmail.com>
2023-03-06 11:36:57 +0000

    added get method for different subject

--- backend/server/app.js
+++ backend/server/app.js
@@ -3,12 +3,26 @@ const cors = require('cors')
 const app = express()
 const logger = require("./logger")
 
+// const quiz = require("ENTER PATH TO QUIZ.JSON HERE")
+
 app.use(cors());
 app.use(express.json());
 app.use(logger);
 
 app.get("/", (req, res) => {
     res.send(`This is the quiz API`)
+});
+
+app.get('/quiz/worldWar', (req, res) => {
+
+})
+
+app.get('/quiz/medieval', (req, res) => {
+
+})
+
+app.get('/quiz/warringStates', (req, res) => {
+
 })
 
 
