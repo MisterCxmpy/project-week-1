64c8315f610edd76ad3e574fefab9e47ff24b480
Steve <123103265+bossSteve03@users.noreply.github.com>
2023-03-08 15:48:58 +0000

    Revert "rolling scoreboard"

--- backend/server/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app.js')
-require('dotenv').config()
-const port = process.env.PORT
-
-app.listen(port, (req, res) => {
-    console.log(`App is now listening on port ${port}`)
-})
--- frontend/javascripts/check-user.js
+++
@@ -1,7 +0,0 @@
-if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
-  localStorage.setItem("username", "");
-}
-
-if (localStorage.getItem("username") == "") {
-  window.location.href = "index.html";
-}
