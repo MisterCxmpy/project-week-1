d7b07d2f36db845aa53d0f06bdca9a483070353e
MisterCxmpy <cosgunhussein@gmail.com>
2023-03-10 11:15:48 +0000

    fix bugs

--- frontend/javascripts/result.js
+++ frontend/javascripts/result.js
@@ -21,7 +21,6 @@ function addToLeaderboard() {
   const era = localStorage.getItem("era");
   const difficulty = localStorage.getItem("difficulty");
 
-  // This leaderboard array is used to store all user results
   leaderboardData.push({
     name: username,
     scores: scores,
@@ -29,7 +28,6 @@ function addToLeaderboard() {
     difficulty: difficulty,
   });
 
-  // This leaderboard array is used to get the most recent user results
   leaderboard.push({
     name: username,
     scores: scores,
@@ -40,7 +38,6 @@ function addToLeaderboard() {
   localStorage.setItem("leaderboard", JSON.stringify(leaderboardData));
 }
 
-// allows us to dynamically create leaderboard values
 function createLeaderboardValue(textContent) {
   const leaderboardValue = document.createElement("span");
   leaderboardValue.classList = "leaderboard-value";
@@ -49,29 +46,18 @@ function createLeaderboardValue(textContent) {
 }
 
 const createLeaderboardTable = () => {
-  // Get the current leaderboard data from local storage
   const _leaderboard = JSON.parse(localStorage.getItem("leaderboard"));
 
-  // Loop through the leaderboard data and add each entry to the table if it matches the current era and difficulty
   for (let i = 0; i < _leaderboard.length; i++) {
     if (_leaderboard[i]["era"] == localStorage.getItem("era") && _leaderboard[i]["difficulty"] == localStorage.getItem("difficulty")){
-
-      // Add the name of the participant to the participants column of the table
       participants.appendChild(createLeaderboardValue(_leaderboard[i]["name"]));
-
-      // Add the score of the participant to the scores column of the table
       scores.appendChild(createLeaderboardValue(_leaderboard[i]["scores"].correctScore));
-
-      // Add the era of the participant to the era column of the table
       era.appendChild(createLeaderboardValue(convertEraNames(_leaderboard[i])));
-
-      // Add the difficulty of the participant to the difficulty column of the table
       difficulty.appendChild(createLeaderboardValue(convertDifficultyNames(_leaderboard[i])));
     };
   }
 };
 
-// correctly formats the names of the era
 const convertEraNames = (leaderboard) => {
   switch (leaderboard["era"]) {
     case "ww1and2":
@@ -85,7 +71,6 @@ const convertEraNames = (leaderboard) => {
   }
 }
 
-// correctly formats the names of the difficulty
 const convertDifficultyNames = (leaderboard) => {
   switch (leaderboard["difficulty"]) {
     case "easy":
@@ -100,28 +85,19 @@ const convertDifficultyNames = (leaderboard) => {
 }
 
 const loadScore = () => {
-  // Call addToLeaderboard function to add the current score to the leaderboard array
   addToLeaderboard();
 
-  // Calculate percentage of correct answers and set a CSS variable to adjust the progress bar
   const percentage = (leaderboard[0]["scores"].correctScore / leaderboard[0]["scores"].questionCount)
   document.documentElement.style.setProperty('--stroke-dashoffset', (600 - 600 * percentage));
 
-  // Display the number of correct answers out of total questions
   number.textContent = `${leaderboard[0]["scores"].correctScore}/${leaderboard[0]["scores"].questionCount}`;
-
-  // Update the era text to display the current era
-  convertEraNames(eraText, leaderboard[0])
   eraText.textContent = `Era: ${convertEraNames(leaderboard[0])}`;
-
-  // Update the difficulty text to display the current difficulty
   difficultyText.textContent = `Difficulty: ${convertDifficultyNames(leaderboard[0])}`;
-
-  // Display the number of missed questions
   questionsMissedText.textContent = `Missed Questions: ${leaderboard[0]["scores"].questionsMissedScore}`;
 
-  // Call createLeaderboardTable function to update the leaderboard table with the latest scores
   createLeaderboardTable();
+
+  localStorage.removeItem("leaderboard")
 };
 
 loadScore();
